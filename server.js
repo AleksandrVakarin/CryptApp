c248152056582b3a060be674626a3dbdb7bbb5f8
AleksandrVakarin <aleksandrvakarin7@gmail.com>
2024-05-23 22:57:27 +0300

    add comments in server.js for deploy app

--- server.js
+++ server.js
@@ -15,3 +15,13 @@ const port = 80
 app.use(express.static('frontend/dist')) // path to dist
 
 app.listen(port, () => console.log('Server has been startedn on port 80 ...'))
+
+// cloud host: terminal: apt udate, apt install git, sudo apt install build-essential checkinstall libssl-dev,
+// terminal: wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | sudo bash
+// terminal: . .bashrc , nvm ls-remote , nvm install 20.12.2(only for this app) , 
+// terminal: git clone https://github.com/AleksandrVakarin/CryptApp.git
+// terminal: cd CryptApp: npm install , cd frontend: npm install
+// terminal: npm run build,
+// cd .. (go to CryptApp)
+// npm install -g pm2
+// pm2 start server.js
